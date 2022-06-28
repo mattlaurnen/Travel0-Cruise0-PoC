21afaf086ffa59830fe69848838c746ec58a877e
mattlaurnen <Mattlaurnen@gmail.com>
2022-06-28 19:20:18 -0400

    Initial commit

---
+++ sample-01/src/utils/contentData.js
@@ -0,0 +1,28 @@
+const contentData = [
+  {
+    title: "Feature #1",
+    link: "https://auth0.com",
+    description:
+      "Feature information bla bla special new features."
+  },
+  {
+    title: "Feature #2",
+    link: "https://auth0.com/docs/multifactor-authentication",
+    description:
+       "Feature information bla bla special new features."
+  },
+  {
+    title: "Feature #3",
+    link: "https://auth0.com/docs/anomaly-detection",
+     description:
+     "Feature information bla bla special new features."
+  },
+  {
+    title:"Feature #4",
+    link: "https://auth0.com/docs/rules",
+     description:
+     "Feature information bla bla special new features."
+  }
+];
+
+export default contentData;
